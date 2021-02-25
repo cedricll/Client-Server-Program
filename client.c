32fb866dd8ba9e2d3b67c92f9bcbdf2d37a1df5d
Cedric Lim <cedriclim99@yahoo.com>
2021-02-24 16:50:53 -0800

    Basic client/server program intialized

---
+++ client.c
@@ -0,0 +1,50 @@
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+
+#define PORT 30000
+
+int main(int argc, char const *argv[]) // ./client server.ics.uci.edu 300000
+{ 
+    printf("argv[1]:%s\n", argv[1]); //server.ics.uci.edu, NEED TO MODIFY CODE to account for this domain. 
+    printf("argv[2]:%s\n", argv[2]); //300000
+    /* TODO:
+        - commands
+            -> Prices(stockName, date) //display stock prices of a stock on a given date
+            -> MaxProfit(stockName) // calc max profit of a share of stock
+            -> quit
+        - handle user inputs!!!
+    */
+    int sock = 0, valread; 
+    struct sockaddr_in serv_addr; 
+    char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+    { 
+        printf("\n Socket creation error \n"); 
+        return -1; 
+    } 
+   
+    serv_addr.sin_family = AF_INET; 
+    serv_addr.sin_port = htons(PORT); 
+       
+    // Convert IPv4 and IPv6 addresses from text to binary form 
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)  
+    { 
+        printf("\nInvalid address/ Address not supported \n"); 
+        return -1; 
+    } 
+   
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+    { 
+        printf("\nConnection Failed \n"); 
+        return -1; 
+    } 
+    send(sock , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    valread = read( sock , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    return 0; 
+} 
---
+++ server.c
@@ -0,0 +1,76 @@
+#include <unistd.h> 
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <stdlib.h> 
+#include <netinet/in.h> 
+#include <string.h> 
+
+#include <sys/types.h>
+#include <netdb.h>
+
+#define PORT 30000
+
+int main(int argc, char const *argv[]) // ./server AAPL.csv TWTR.csv 30000
+{ 
+    printf("argv[1]:%s\n", argv[1]); //AAPL.CSV
+    printf("argv[2]:%s\n", argv[2]); //TWTR.csv
+    printf("argv[3]:%s\n", argv[3]); //30000
+    /*TODO:
+        1.) read from apple and twitter files; 7/2/2018 - 6/30/2020
+            -> interested 1st and 5th column
+            -> gaps in data
+            -> store in a dict-like data structure?
+        2.) start listening to client reuqests
+    */
+
+    printf("server started\n");
+
+    int server_fd, new_socket, valread; 
+    struct sockaddr_in address; 
+    int opt = 1; 
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    char *hello = "Hello from server"; 
+       
+    // Creating socket file descriptor 
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
+    { 
+        perror("socket failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, // SO_REUSEPORT
+                                                  &opt, sizeof(opt))) 
+    { 
+        perror("setsockopt"); 
+        exit(EXIT_FAILURE); 
+    } 
+    address.sin_family = AF_INET; 
+    address.sin_addr.s_addr = INADDR_ANY; 
+    address.sin_port = htons( PORT ); 
+       
+    // Forcefully attaching socket to the port 8080 
+    if (bind(server_fd, (struct sockaddr *)&address,  
+                                 sizeof(address))<0) 
+    { 
+        perror("bind failed"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if (listen(server_fd, 3) < 0) 
+    { 
+        perror("listen"); 
+        exit(EXIT_FAILURE); 
+    } 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
+                       (socklen_t*)&addrlen))<0) 
+    { 
+        perror("accept"); 
+        exit(EXIT_FAILURE); 
+    } 
+    valread = read( new_socket , buffer, 1024); 
+    printf("%s\n",buffer ); 
+    send(new_socket , hello , strlen(hello) , 0 ); 
+    printf("Hello message sent\n"); 
+    return 0; 
+} 
